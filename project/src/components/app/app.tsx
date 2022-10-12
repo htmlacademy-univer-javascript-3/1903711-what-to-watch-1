51a37ed12cd809d19b8423a3920b2faed5414926
kekis01 <Vladislav.Zekunov@urfu.me>
2022-10-12 22:37:10 +0500

    Добавил пути для ссылок

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,13 @@
+import {AppRoute, AuthorizationStatus } from '../../const';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from '../../pages/main/main';
+import SignIn from '../../pages/sign-in/sign-in';
+import Film from '../../pages/film/film';
+import Player from '../../pages/player/player';
+import AddReview from '../../pages/add-review/add-review';
+import MyList from '../../pages/my-list/my-list';
+import NotFound from '../../pages/not-found/not-found';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   title: string,
@@ -7,7 +16,48 @@ type AppScreenProps = {
 }
 
 function App({title, genre, date}: AppScreenProps): JSX.Element {
-  return <Main title={title} genre={genre} date={date} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main title={title} genre={genre} date={date} />}
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignIn />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<Film />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<Player />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <AddReview />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.NoAuth}>
+              <MyList />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
